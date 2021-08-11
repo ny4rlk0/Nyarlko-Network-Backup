3dafb4c2e63c115a923296756426f40f23aec07c
ny4rlk0 <55476474+ny4rlk0@users.noreply.github.com>
2021-08-11 20:39:49 +0300

    Update nyabackup.py

--- nyabackup.py
+++ nyabackup.py
@@ -1,8 +1,10 @@
 #Nyarlko Network Backup Service
 #https://github.com/ny4rlk0/Nyarlko-Network-Backup/
 from posixpath import basename;from zipfile import ZipFile;import os;from datetime import date;from datetime import datetime;from shutil import copy2;import subprocess as sp;
-user="enter_your_username";password="enter_your_password";networkPath=f"\\\\192.168.1.1\\unity\\Projects"
-dirName="C://Users//Guest//Desktop//SHARES";backupName="Backup"
+user="enter_your_username";password="enter_your_password"
+networkPath=f"\\\\192.168.1.1\\unity\\Projects" #Network Path
+dirName="C://Users//Guest//Desktop//SHARES" #Source folder to backup
+backupName="Backup" #Backup Name
 def today():
     now = datetime.now()
     clock=now.strftime("%H.%M")
