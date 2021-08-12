b8b32713905edd4231d9f8b9d068507c2afa81b8
ny4rlk0 <55476474+ny4rlk0@users.noreply.github.com>
2021-08-12 23:53:36 +0300

    Update and rename nyabackupGUI.py to nyabackuprestoreGUI.py

--- nyabackuprestoreGUI.py
+++ nyabackuprestoreGUI.py
@@ -1,8 +1,8 @@
-#Nyarlko Network Backup Service GUI
-from posixpath import basename;import sys;from threading import Thread as core;import time;from tkinter import *;import configparser as cfg;from zipfile import ZipFile;import zipfile;import os;from datetime import date;from datetime import datetime;from shutil import copy2;import subprocess as sp;
+#Nyarlko Network Backup / Restore Service GUI
+from posixpath import basename;import sys;from tkinter.ttk import *;from threading import Thread as core;import time;from tkinter import *;import configparser as cfg;from zipfile import ZipFile;import zipfile;import os;from datetime import date;from datetime import datetime;from shutil import copy2;import subprocess as sp;
 user="enter_your_username";password="enter_your_password";networkPath=f"\\\\192.168.1.1\\unity\\Projects" #Network Path
 dirName="C://Users//Guest//Desktop//SHARES" #Source folder to backup
-backupName="[NABS] Backup";compression = zipfile.ZIP_DEFLATED
+backupName="[NABS] Backup";compression = zipfile.ZIP_DEFLATED ;backup_list=[]
 w=Tk();w.title('Nyarlko Automated Backup System');w.configure(background='black');status='Idle.'
 def readConfig():
     global user,password,networkPath,dirName,backupName,stat
@@ -13,8 +13,8 @@ def readConfig():
     networkPath=str(r['config'] ['networkPath'])
     dirName=str(r['config']['dirName'])
     backupName=str(r['config']['backupName'])
-    usre.delete(0,END);pwe.delete(0,END);npe.delete(0,END);dne.delete(0,END);bne.delete(0,END) #clear boxes
-    usre.insert(0, user);pwe.insert(0, password);npe.insert(0, networkPath);dne.insert(0, dirName);bne.insert(0, backupName) #fill boxes
+    usre.delete(0,END);pwe.delete(0,END);npe.delete(0,END);dne.delete(0,END);bne.delete(0,END)
+    usre.insert(0, user);pwe.insert(0, password);npe.insert(0, networkPath);dne.insert(0, dirName);bne.insert(0, backupName)
 def createExampleConfig():
     global stat
     r=cfg.RawConfigParser()
@@ -47,16 +47,20 @@ def today():
     month=date.today().strftime("%B")
     year=date.today().strftime("%Y")
     return digitday,day,month,year,clock
-def upload_file(backupPath):
+def access_network_drive():
     global stat,backupButton,saveConfigButton
     n=sp.DEVNULL;stat["text"]="Deleting active network shares...";time.sleep(4)
     sp.Popen("NET USE * //delete",stdout=n,stderr=n,stdin=n)
     accessNetworkDrive='NET USE '+networkPath+' /User:'+user+' '+password
-    stat["text"] = "Uploading backup..";time.sleep(4)
+    stat["text"] = "Accessing network drive..";time.sleep(4)
     sp.Popen(accessNetworkDrive,stdout=n,stderr=n,stdin=n)
+def upload_file(backupPath):
+    global stat,backupButton,saveConfigButton,restoreButton
+    access_network_drive();time.sleep(2)
+    stat["text"] = "Uploading backup..";time.sleep(4)
     copy2(backupPath,networkPath)
     stat["text"] = "Upload finished...";time.sleep(4)
-    backupButton["state"]="active";saveConfigButton["state"]="active"
+    backupButton["state"]="active";saveConfigButton["state"]="active";restoreButton["state"]="active"
 def compress_folder():
     global stat
     fileNumber=0
@@ -71,12 +75,51 @@ def compress_folder():
     backupPath=os.getcwd()+"\\"+backupname
     stat["text"] = "Compression finished..";time.sleep(4)
     upload_file(backupPath)
-def daemon_handler():
-    global backupButton,saveConfigButton,stat
+def list_backups():
+    global backup_list,backupComboboxList
+    backup_list=[]
+    selected_backup_name=backupComboboxList.get()
+    selected_backup=os.path.join(networkPath, selected_backup_name)
+    backup_exists=os.path.exists(selected_backup)
+    if not backup_exists:
+        backupComboboxList.delete(0,END)
+    access_network_drive()
+    for file in os.listdir(networkPath): #Find backup files ending with .zip in networkDirectory
+        if file.endswith(".zip"):
+            backup_list.append(file)
+    for b in backup_list: #Update Combobox list
+        backupComboboxList["values"] = backup_list
+def restore():
+    global backupComboboxList,backupButton,saveConfigButton,restoreButton,stat
+    list_backups()
+    selected_backup_name=backupComboboxList.get()
+    selected_backup=os.path.join(networkPath, selected_backup_name)
+    copy_dir=dirName+"//"+selected_backup_name
+    zip_dir=dirName+"//"+selected_backup_name+"//"+selected_backup_name
+    copydir_exists=os.path.exists(copy_dir)
+    if not copydir_exists:
+        os.mkdir(copy_dir)
+    backup_exists=os.path.exists(selected_backup)
+    if backup_exists:
+        copy2(selected_backup,copy_dir)
+        with zipfile.ZipFile(zip_dir, 'r') as z:
+            z.extractall(copy_dir)
+        os.remove(zip_dir)
+        stat["text"] = "Backup restored";time.sleep(2)
+    else:stat["text"] = "Backup not found";time.sleep(2)
+    backupButton["state"]="active";saveConfigButton["state"]="active";restoreButton["state"]="active"
+def upload_proccess_handler():
+    global backupButton,saveConfigButton,restoreButton,stat
     stat["text"] = "Compressing folder.."
-    backupButton["state"] = "disabled";saveConfigButton["state"] = "disabled"
+    backupButton["state"] = "disabled";saveConfigButton["state"] = "disabled";restoreButton["state"] = "disabled"
     compress_proccess=core(target=compress_folder);compress_proccess.daemon=True 
     compress_proccess.start()
+def restore_proccess_handler():
+    global backupButton,saveConfigButton,restoreButton,stat
+    stat["text"] = "Restoring from backup.."
+    backupButton["state"] = "disabled";saveConfigButton["state"] = "disabled";restoreButton["state"] = "disabled"
+    restore_proccess=core(target=restore);restore_proccess.daemon=True 
+    restore_proccess.start()
 #UserBox
 usr=Label (w, text="Username:" ,bg="black",fg="white",font="none 12 bold");usr.grid(row=0,column=0,sticky=W)
 usre=Entry(w,width=40,bg="white");usre.grid(row=0,column=1,sticky=W)
@@ -92,17 +135,24 @@ dne=Entry(w,width=40,bg="white");dne.grid(row=3,column=1,sticky=W)
 #BackupName
 bn=Label (w, text="Backup Name:" ,bg="black",fg="white",font="none 12 bold");bn.grid(row=4,column=0,sticky=W)
 bne=Entry(w,width=40,bg="white");bne.grid(row=4,column=1,sticky=W)
+#BackupList
+#variable=StringVar(w)
+backupComboboxList=Combobox(w,width=65,values=list_backups,state="readonly")
+backupComboboxList.grid(row=7,column=1)
 #BackupButton
-backupButton=Button(w,text="Backup",width=12,font="none 12 bold",command=daemon_handler)
-backupButton.grid(row=7,column=0,sticky=W)
+backupButton=Button(w,text="Backup",width=12,font="none 12 bold",command=upload_proccess_handler)
+backupButton.grid(row=8,column=0,sticky=W)
 #ConfigSaveButton
 saveConfigButton=Button(w,text="Save Config",width=12,font="none 12 bold",command=writeConfig)
-saveConfigButton.grid(row=7,column=1,sticky=W)
+saveConfigButton.grid(row=8,column=1,sticky=W)
+#RestoreButton
+restoreButton=Button(w,text="Restore",width=12,font="none 12 bold",command=restore_proccess_handler)
+restoreButton.grid(row=8,column=2,sticky=W)
 #Status
-stat=Label (w, text=status ,bg="black",fg="white",font="none 12 bold");stat.grid(row=8,column=0)
+stat=Label (w, text=status ,bg="black",fg="white",font="none 12 bold");stat.grid(row=9,column=0)
 settings_exists=os.path.exists(os.getcwd()+'\\nbs.settings')
 if not settings_exists:createExampleConfig();readConfig()
-else:readConfig()
+else:readConfig();list_backups()
 #Run the main loop
 w.mainloop()
 sys.exit(0)
