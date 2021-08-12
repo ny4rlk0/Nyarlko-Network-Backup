dc93cdc63afc3f80783a01a16a754ae898ec0127
ny4rlk0 <55476474+ny4rlk0@users.noreply.github.com>
2021-08-12 20:52:21 +0300

    Update nyabackupGUI.py

--- nyabackupGUI.py
+++ nyabackupGUI.py
@@ -13,8 +13,8 @@ def readConfig():
     networkPath=str(r['config'] ['networkPath'])
     dirName=str(r['config']['dirName'])
     backupName=str(r['config']['backupName'])
-    stat["text"] = "Settings loaded."
-    return user,password,networkPath,dirName,backupName
+    usre.delete(0,END);pwe.delete(0,END);npe.delete(0,END);dne.delete(0,END);bne.delete(0,END)
+    usre.insert(0, user);pwe.insert(0, password);npe.insert(0, networkPath);dne.insert(0, dirName);bne.insert(0, backupName)
 def createExampleConfig():
     global stat
     r=cfg.RawConfigParser()
@@ -32,16 +32,13 @@ def writeConfig():
     global stat
     r=cfg.RawConfigParser()
     r.add_section('config')
-    r.set('config','user',str(t.get()))
-    r.set('config','password',str(t1.get()))
-    r.set('config','networkPath',str(t2.get()))
-    r.set('config','dirName',str(t3.get()))
-    r.set('config','backupName',str(t4.get()))
-    makeConfig=open('nbs.settings','w')
-    r.write(makeConfig)
-    makeConfig.close()
-    stat["text"] = "Settings saved."
-    readConfig()
+    r.set('config','user',str(usre.get()))
+    r.set('config','password',str(pwe.get()))
+    r.set('config','networkPath',str(npe.get()))
+    r.set('config','dirName',str(dne.get()))
+    r.set('config','backupName',str(bne.get()))
+    makeConfig=open('nbs.settings','w');r.write(makeConfig);makeConfig.close()
+    stat["text"]="Settings saved.";time.sleep(4);readConfig()
 def today():
     now = datetime.now()
     clock=now.strftime("%H.%M")
@@ -52,19 +49,16 @@ def today():
     return digitday,day,month,year,clock
 def upload_file(backupPath):
     global stat,backupButton,saveConfigButton
-    stat["text"] = "Deleting active network shares..."
-    time.sleep(4)
-    sp.Popen("NET USE * //delete", stdout=sp.DEVNULL, stderr = sp.DEVNULL , stdin = sp.DEVNULL)
-    accessNetworkDrive = 'NET USE ' + networkPath + ' /User:' + user + ' ' + password
-    stat["text"] = "Uploading backup.."
-    time.sleep(4)
-    sp.Popen(accessNetworkDrive, stdout=sp.DEVNULL, stderr = sp.DEVNULL , stdin = sp.DEVNULL)
+    n=sp.DEVNULL;stat["text"]="Deleting active network shares...";time.sleep(4)
+    sp.Popen("NET USE * //delete",stdout=n,stderr=n,stdin=n)
+    accessNetworkDrive='NET USE '+networkPath+' /User:'+user+' '+password
+    stat["text"] = "Uploading backup..";time.sleep(4)
+    sp.Popen(accessNetworkDrive,stdout=n,stderr=n,stdin=n)
     copy2(backupPath,networkPath)
-    stat["text"] = "Upload finished..."
-    time.sleep(4)
-    backupButton["state"] = "active"
-    saveConfigButton["state"] = "active"
+    stat["text"] = "Upload finished...";time.sleep(4)
+    backupButton["state"]="active";saveConfigButton["state"]="active"
 def compress_folder():
+    global stat
     fileNumber=0
     backupname=backupName+" "+str(today())+".zip"
     with ZipFile (backupname,'w') as zip:
@@ -75,55 +69,40 @@ def compress_folder():
                 zip.write(filePath,basename(filePath),compress_type=compression)
                 fileNumber=fileNumber+1
     backupPath=os.getcwd()+"\\"+backupname
-    status='Compression Finished!'
-    time.sleep(2)
+    stat["text"] = "Compression finished..";time.sleep(4)
     upload_file(backupPath)
 def daemon_handler():
     global backupButton,saveConfigButton,stat
     stat["text"] = "Compressing folder.."
-    backupButton["state"] = "disabled"
-    saveConfigButton["state"] = "disabled"
-    compress_proccess = core(target=compress_folder)#, args=(1,)) Flask Server.
-    compress_proccess.daemon=True #To kill when its needed neccesary
-    compress_proccess.start() #Flask sunucusunu ayrı bir çekirdekte çalıştır.
+    backupButton["state"] = "disabled";saveConfigButton["state"] = "disabled"
+    compress_proccess=core(target=compress_folder);compress_proccess.daemon=True 
+    compress_proccess.start()
 #UserBox
-Label (w, text="Username:" ,bg="black",fg="white",font="none 12 bold") .grid(row=0,column=0,sticky=W)
-t=Entry(w,width=40,bg="white")
-t.insert(0, user)
-t.grid(row=0,column=1,sticky=W)
+usr=Label (w, text="Username:" ,bg="black",fg="white",font="none 12 bold");usr.grid(row=0,column=0,sticky=W)
+usre=Entry(w,width=40,bg="white");usre.grid(row=0,column=1,sticky=W)
 #PasswordBox
-Label (w, text="Password:" ,bg="black",fg="white",font="none 12 bold") .grid(row=1,column=0,sticky=W)
-t1=Entry(w,width=40,bg="white")
-t1.insert(0, password)
-t1.grid(row=1,column=1,sticky=W)
+pw=Label (w, text="Password:" ,bg="black",fg="white",font="none 12 bold");pw.grid(row=1,column=0,sticky=W)
+pwe=Entry(w,width=40,bg="white");pwe.grid(row=1,column=1,sticky=W)
 #NetworkBox
-Label (w, text="Network Path:" ,bg="black",fg="white",font="none 12 bold") .grid(row=2,column=0,sticky=W)
-t2=Entry(w,width=40,bg="white")
-t2.insert(0, networkPath)
-t2.grid(row=2,column=1,sticky=W)
+np=Label (w, text="Network Path:" ,bg="black",fg="white",font="none 12 bold");np.grid(row=2,column=0,sticky=W)
+npe=Entry(w,width=40,bg="white");npe.grid(row=2,column=1,sticky=W)
 #DirectoryNameBox
-Label (w, text="Directory:" ,bg="black",fg="white",font="none 12 bold") .grid(row=3,column=0,sticky=W)
-t3=Entry(w,width=40,bg="white")
-t3.insert(0, dirName)
-t3.grid(row=3,column=1,sticky=W)
+dn=Label (w, text="Directory:" ,bg="black",fg="white",font="none 12 bold");dn.grid(row=3,column=0,sticky=W)
+dne=Entry(w,width=40,bg="white");dne.grid(row=3,column=1,sticky=W)
 #BackupName
-Label (w, text="Backup Name:" ,bg="black",fg="white",font="none 12 bold") .grid(row=4,column=0,sticky=W)
-t4=Entry(w,width=40,bg="white")
-t4.insert(0, backupName)
-t4.grid(row=4,column=1,sticky=W)
-global backupButton,saveConfigButton,stat
-#Backup Button
+bn=Label (w, text="Backup Name:" ,bg="black",fg="white",font="none 12 bold");bn.grid(row=4,column=0,sticky=W)
+bne=Entry(w,width=40,bg="white");bne.grid(row=4,column=1,sticky=W)
+#BackupButton
 backupButton=Button(w,text="Backup",width=12,font="none 12 bold",command=daemon_handler)
 backupButton.grid(row=7,column=0,sticky=W)
-#DosyaKaydetButton
+#ConfigSaveButton
 saveConfigButton=Button(w,text="Save Config",width=12,font="none 12 bold",command=writeConfig)
 saveConfigButton.grid(row=7,column=1,sticky=W)
-#Durum
-stat=Label (w, text=status ,bg="black",fg="white",font="none 12 bold")
-stat.grid(row=8,column=0)
-#Run the main loop
+#Status
+stat=Label (w, text=status ,bg="black",fg="white",font="none 12 bold");stat.grid(row=8,column=0)
 settings_exists=os.path.exists(os.getcwd()+'\\nbs.settings')
-if not settings_exists:createExampleConfig()
+if not settings_exists:createExampleConfig();readConfig()
 else:readConfig()
+#Run the main loop
 w.mainloop()
 sys.exit(0)
